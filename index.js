6bd2d06c7647156ae0c60a28983329dfe379d964
FahadAhmed-8 <fahadahmed982004@gmail.com>
2024-12-23 20:57:25 +0530

    add to github

---
+++ index.js
@@ -0,0 +1,75 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+app.use(express.json())
+
+let teaData = []
+let nextId = 1
+
+//add new tea
+app.post('/teas', (req, res) => {
+    const {name, price} = req.body;
+    const newTea = {id: nextId++, name, price};
+    teaData.push(newTea);
+    res.status(201).send(newTea);
+})
+
+//add all tea
+app.get('/teas', (req, res) => {
+    res.status(200).send(teaData);
+})
+
+//find a tea with id
+app.get('/teas/:id', (req, res) => {
+    const id = req.params.id
+    const tea = teaData.find(t => t.id === parseInt(id))
+    if (!tea){
+        return res.status(404).send('tea not found')
+    }
+    res.status(200).send(tea)
+})
+
+//update tea
+app.put('/teas/:id', (req, res) => {
+    const id = req.params.id
+    const tea = teaData.find(t => t.id === parseInt(id))
+    if (!tea){
+        return res.status(404).send('tea not found')
+    }
+    
+    const {name, price} = req.body
+    tea.name = name
+    tea.price = price
+    res.status(200).send(tea)    
+})
+
+//delete tea
+app.delete('/teas/:id', (req, res) => {
+    const id = req.params.id
+    const index = teaData.findIndex(t => t.id === parseInt(id))
+    if (index === -1){
+        return res.status(404).send('tea not found')
+    }
+    
+    teaData.splice(index, 1)
+    res.status(204).send('tea deleted')
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}...`)
+})
+
+ 
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+// app.get('/india', (req, res) => {
+//     res.send('Hello INDIA!')
+// })
+
+// app.get('/katni', (req, res) => {
+//     res.send('Hello Katni!')
+// })
